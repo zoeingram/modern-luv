bb2ebb78e22c5063279353fecb4bffdf0303df80
Zoe Ingram <zoe.a.ingram@gmail.com>
2017-11-29 11:51:39 -0800

    adds structure

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 3000
+
+app()
+  .use(app.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
